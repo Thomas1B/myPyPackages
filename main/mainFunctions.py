e9354015c84f5fed72778bc315d7930f96f14abb
Thomas <thomasbourgeois2@hotmail.com>
2023-01-02 22:21:54 -0800

    formatting

--- main/mainFunctions.py
+++ main/mainFunctions.py
@@ -7,92 +7,6 @@ import inspect
 from termcolor import colored
 
 
-def showModules(moduleName):
-    '''
-    Function to print names of modules
-        (also prints docstring of function if passed a function name)
-
-    Parameter:
-        modules (str): name of module (or function)
-
-    Returns:
-        prints list of functions
-         (or docstring of a function)
-    '''
-
-    if callable(moduleName):
-        print("Docstring for {}:".format(moduleName.__name__))
-        print(moduleName.__doc__)
-    else:
-        print("List of available functions in {}".format(moduleName.__name__))
-        for i, func in enumerate(moduleName.func_list):
-            print("   {}: {}".format(i+1, func.__name__))
-
-
-def see_callerLoc(do_not_delete, s=None, full=False, quit=False):
-    '''
-    Function to see where something was called from, using the keyboard module. 
-        Made for debugging.
-
-    To use type: see_caller(inspect.currentframe())
-        *** The first parameter must always be called like this ***
-
-    Parameter: 
-        frame: inspect.currentframe().
-        s (str) [optional]: extra text to pring inside caller.
-        full (bool) [optional, default - filename] : bool whether to show full filepath or just the filename.
-        quit (bool) [optioanl, default - False] : bool to terminate program.
-
-    '''
-    frame_info = inspect.getframeinfo(do_not_delete)
-    path = frame_info.filename
-    line = frame_info.lineno
-
-    print(color_txt("Caller Location", color="red", styles=['underline']))
-    tmp = path.split("\\")[-1]
-    tmp_txt = f'Filename: {tmp}\n'
-    if full:
-        tmp_txt = f'Full filepath: {path}\n'
-
-    txt = f'{tmp_txt}\nCalled on line {line}.'
-    print(txt, '\n')
-    if s:  # if user passed extra text to print.
-        print(s, '\n')
-    if quit == True:
-        print("Some IDEs may need to closed and reopen to start the program...", '\n')
-        os._exit(os.X_OK)
-
-
-def clear_screen():
-    '''
-    Function to clear the terminal screen.
-    '''
-    os.system('clear')
-
-
-def quit_program(check=False):
-    '''
-    Function to terminate program.
-
-    Parameter:
-        check (bool): double check if user really wants to quit the program (default False).
-    '''
-    user = input('Exit program? (y/n): ')
-    if user == 'y':
-        if check:
-            user = input("Are you sure? (y/n): ")
-            if user == 'y':
-                print("\nProgram Closing...")
-                time.sleep(0.250)
-                exit(True)
-        else:
-            print("\nProgram Closing...")
-            time.sleep(0.250)
-            exit(True)
-    else:
-        return False
-
-
 def color_txt(s, color=None, highlight=None, attrs=None):
     '''
     Function to color text using termcolor module.
@@ -200,6 +114,92 @@ def warning(color='red', highlight='yellow'):
     Initially designed for debugging.
     '''
     print_color("*** Warning ***", color=color, highlight=highlight)
+
+
+def clear_screen():
+    '''
+    Function to clear the terminal screen.
+    '''
+    os.system('clear')
+
+
+def showModules(moduleName):
+    '''
+    Function to print names of modules
+        (also prints docstring of function if passed a function name)
+
+    Parameter:
+        modules (str): name of module (or function)
+
+    Returns:
+        prints list of functions
+         (or docstring of a function)
+    '''
+
+    if callable(moduleName):
+        print("Docstring for {}:".format(moduleName.__name__))
+        print(moduleName.__doc__)
+    else:
+        print("List of available functions in {}".format(moduleName.__name__))
+        for i, func in enumerate(moduleName.func_list):
+            print("   {}: {}".format(i+1, func.__name__))
+
+
+def see_callerLoc(do_not_delete, s=None, full=False, quit=False):
+    '''
+    Function to see where something was called from, using the keyboard module. 
+        Made for debugging.
+
+    To use type: see_caller(inspect.currentframe())
+        *** The first parameter must always be called like this ***
+
+    Parameter: 
+        frame: inspect.currentframe().
+        s (str) [optional]: extra text to pring inside caller.
+        full (bool) [optional, default - filename] : bool whether to show full filepath or just the filename.
+        quit (bool) [optioanl, default - False] : bool to terminate program.
+
+    '''
+    frame_info = inspect.getframeinfo(do_not_delete)
+    path = frame_info.filename
+    line = frame_info.lineno
+
+    print(color_txt("Caller Location", color="red", styles=['underline']))
+    tmp = path.split("\\")[-1]
+    tmp_txt = f'Filename: {tmp}\n'
+    if full:
+        tmp_txt = f'Full filepath: {path}\n'
+
+    txt = f'{tmp_txt}\nCalled on line {line}.'
+    print(txt, '\n')
+    if s:  # if user passed extra text to print.
+        print(s, '\n')
+    if quit == True:
+        print("Some IDEs may need to closed and reopen to start the program...", '\n')
+        os._exit(os.X_OK)
+
+
+def quit_program(check=False):
+    '''
+    Function to terminate program.
+
+    Parameter:
+        check (bool): double check if user really wants to quit the program (default False).
+    '''
+    user = input('Exit program? (y/n): ')
+    if user == 'y':
+        if check:
+            user = input("Are you sure? (y/n): ")
+            if user == 'y':
+                print("\nProgram Closing...")
+                time.sleep(0.250)
+                exit(True)
+        else:
+            print("\nProgram Closing...")
+            time.sleep(0.250)
+            exit(True)
+    else:
+        return False
 
 
 def check_matches(f1, f2):
